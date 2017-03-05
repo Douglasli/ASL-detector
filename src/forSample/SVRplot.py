f72ee0413bd983bb93892b43239e8fa02a2289b3
Douglas Lee <a811278305@gmail.com>
2017-03-06 01:23:04 +0800

    test

--- src/forSample/SVRplot.py
+++ src/forSample/SVRplot.py
@@ -21,27 +21,29 @@ clf=joblib.load("../predict/machine_SVR.pkl")
 
 predict=clf.predict(X)
 print clf.score(X, y)
-fig = plt.figure(figsize=(10, 5), dpi=200)
+scores = cross_val_score(clf, X, y, cv=10)
+print scores
+# fig = plt.figure(figsize=(10, 5), dpi=200)
 
-plt.scatter(predict,y,s=2,color='red')
-for a in range(1,37):
-	plt.plot([a-0.5,a+0.5], [a,a],color='blue',linestyle="-")
-#annotate
-datalist = {'a':1,'b':2,'c':3,'k':4,'e':5,'f':6,'g':7,'h':8,'i':9,'j':10,'d':11,'l':12,'m':13,'n':14,'o':15,'p':16,'q':17,'r':18,'s':19,'t':20,'u':21,'v':22,'w':23,'x':24,'y':25,'z':26,'0':27,'1':28,'2':29,'3':30,'4':31,'5':32,'6':33,'7':34,'8':35,'9':36}
-for a in range(1,37):
-	text = datalist.keys()[datalist.values().index(a)]
-	plt.annotate(text,
- 	            xy=(a, a), xycoords='data',xytext=(+0, +3), textcoords='offset points',fontsize=10)
+# plt.scatter(predict,y,s=2,color='red')
+# for a in range(1,37):
+# 	plt.plot([a-0.5,a+0.5], [a,a],color='blue',linestyle="-")
+# #annotate
+# datalist = {'a':1,'b':2,'c':3,'k':4,'e':5,'f':6,'g':7,'h':8,'i':9,'j':10,'d':11,'l':12,'m':13,'n':14,'o':15,'p':16,'q':17,'r':18,'s':19,'t':20,'u':21,'v':22,'w':23,'x':24,'y':25,'z':26,'0':27,'1':28,'2':29,'3':30,'4':31,'5':32,'6':33,'7':34,'8':35,'9':36}
+# for a in range(1,37):
+# 	text = datalist.keys()[datalist.values().index(a)]
+# 	plt.annotate(text,
+#  	            xy=(a, a), xycoords='data',xytext=(+0, +3), textcoords='offset points',fontsize=10)
 
-#configure  X axes
-plt.xlim(0,38)
-plt.xticks([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37])
+# #configure  X axes
+# plt.xlim(0,38)
+# plt.xticks([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37])
 
-#configure  Y axes
-plt.ylim(0,38)
-plt.yticks([1,4,8,12,16,20,24,28,32,36,37])
+# #configure  Y axes
+# plt.ylim(0,38)
+# plt.yticks([1,4,8,12,16,20,24,28,32,36,37])
 
-#label
-plt.xlabel('Predicted')
-plt.ylabel('Measured')
-plt.show()
+# #label
+# plt.xlabel('Predicted')
+# plt.ylabel('Measured')
+# plt.savefig('foo.png')
